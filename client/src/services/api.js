e556a9263bdf8a41be8c9918b1e25ea3d4707e5c
Nikhil khare <143277827+NikhilKhare973@users.noreply.github.com>
2026-07-02 18:02:16 +0530

    Update base URL for API requests

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://smart-warehouse-inventory.onrender.com/api', // Pointing to your Node Express server
+    baseURL: 'https://smart-warehouse-inventory.onrender.com/', // Pointing to your Node Express server
 });
 
 // Automatically attach the JWT token to every outgoing request
